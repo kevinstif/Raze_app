7f9ab7ac5103e2da1f664dd4c7f0ab79b0883225
Will-iam210 <axeso_william@hotmail.com>
2021-09-29 19:47:21 -0500

    update-components

---
+++ app_web/src/components/Login.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="login-container">
+    <button @click="loginSubmit">Login with Google</button>
+  </div>
+</template>
+
+<script>
+import firebase from "firebase/compat";
+export default {
+  methods: {
+    loginSubmit() {
+      const provider = new firebase.auth.GoogleAuthProvider()
+      firebase.auth().signInWithRedirect(provider)
+          .catch(console.log)
+    }
+  }
+}
+</script>
--- app_web/src/main.js
+++ app_web/src/main.js
@@ -2,11 +2,25 @@ import Vue from 'vue'
 import App from './App.vue'
 import vuetify from './plugins/vuetify'
 import router from './router'
+import firebase from "firebase/compat";
 
 Vue.config.productionTip = false
 
-new Vue({
+const firebaseConfig = {
+  apiKey: "AIzaSyCtTL1UiQe0eATjGpFOVhFF5c5i34j3qAo",
+  authDomain: "raze-chat-37c27.firebaseapp.com",
+  projectId: "raze-chat-37c27",
+  storageBucket: "raze-chat-37c27.appspot.com",
+  messagingSenderId: "286184256621",
+  appId: "1:286184256621:web:3e46bea1b6e5a42f1afb64"
+};
+
+firebase.initializeApp(firebaseConfig)
+
+firebase.auth().onAuthStateChanged(() => new Vue({
   vuetify,
   router,
   render: h => h(App)
-}).$mount('#app')
+}).$mount('#app'))
+
+
