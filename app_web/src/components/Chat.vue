a3aace5538d5d8e9b2df7fa9f4a2e788def33254
Jesús Emerson Pacheco Alanya <jesuspacheco0219@gmail.com>
2021-09-29 20:46:27 -0500

    Update Chat.vue

--- app_web/src/components/Chat.vue
+++ app_web/src/components/Chat.vue
@@ -1,3 +1,47 @@
 <template>
-    
+     <!--Jesus-->
+  <div class="wrapper">
+    <header>
+      <h1>Raze Chat</h1>
+      <button @click="logout">Logout</button>
+    </header>
+    <!--Jesus-->
 </template>
+
+<style scoped>
+/*Yisus*/
+.wrapper section main::-webkit-scrollbar {
+  width: 0.25rem;
+}
+.wrapper section main::-webkit-scrollbar-track {
+  background: #1e1e24;
+}
+.wrapper section main::-webkit-scrollbar-thumb {
+  background: #6649b8;
+}
+.wrapper section form {
+  height: 10vh;
+  position: fixed;
+  bottom: 0;
+  background-color: #181717;
+  width: 100%;
+  display: flex;
+  font-size: 1.5rem;
+  border: 2px solid grey;
+}
+.wrapper section form button {
+  width: 20%;
+  background-color: #F7444E;
+}
+.wrapper section form input {
+  line-height: 1.5;
+  width: 100%;
+  font-size: 1.5rem;
+  background: #3a3a3a;
+  color: white;
+  outline: none;
+  border: none;
+  padding: 0 10px;
+}
+/*Yisus*/
+</style>
