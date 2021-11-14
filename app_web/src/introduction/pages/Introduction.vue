e39f8775f5f45efa4cfab15989254cd29bc8a24e
Jesús Emerson Pacheco Alanya <jesuspacheco0219@gmail.com>
2021-11-14 00:14:38 -0500

    add colum

--- app_web/src/introduction/pages/Introduction.vue
+++ app_web/src/introduction/pages/Introduction.vue
@@ -1,7 +1,56 @@
 <template>
-  <div>
-    Enter Intdoduction
-  </div>
+    <v-content class="bak">
+      <v-container class="fill-height" fluid>
+        <v-row align="center" justify="center">
+          <v-col cols="12" sm="8" md="8">
+            <v-card class="elevation-12">
+              <v-window v-model="step">
+                <v-window-item :value="1">
+                  <v-row>
+                    <v-col cols="12" md="4" class="colum">
+                    </v-col>
+                    <v-col cols="12" md="8">
+                      <v-card-text class="mt-12">
+                        <h1
+                            class="letter"
+                        >Welcome To Raze</h1>
+                        <h4 class="text-center mt-4">
+                          Enter your details
+                        </h4>
+                        <v-form>
+                          <v-text-field
+                              v-model="signEmail"
+                              label="Email"
+                              name="Email"
+                              prepend-icon="email"
+                              type="text"
+                              color="teal accent-3"
+                          />
+
+                          <v-text-field
+                              v-model="signPassword"
+                              id="password"
+                              label="Password"
+                              name="password"
+                              prepend-icon="lock"
+                              type="password"
+                              color="teal accent-3"
+                          />
+                        </v-form>
+                        <h3 class="text-center mt-4">Forgot your password ?</h3>
+                      </v-card-text>
+                      <div class="text-center mt-3">
+                        <v-btn to="/Web/Posts" rounded color="teal accent-3" dark >SIGN IN</v-btn>
+                      </div>
+                    </v-col>
+                  </v-row>
+                </v-window-item>
+              </v-window>
+            </v-card>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-content>
 </template>
 
 <script>
--- app_web/src/login/pages/Register.vue
+++ app_web/src/login/pages/Register.vue
@@ -1,7 +1,5 @@
 
 <template>
-
-  <v-app class="bak">
     <v-content class="bak">
       <v-container class="fill-height" fluid>
         <v-row align="center" justify="center">
@@ -134,7 +132,6 @@
         </v-row>
       </v-container>
     </v-content>
-  </v-app>
 </template>
 
 
